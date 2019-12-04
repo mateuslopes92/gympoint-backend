9717a561b09c2b507adb60f9e8584972dbe94d3d
mateuslopes92 <mateuslopes92@gmail.com>
2019-12-04 13:14:32 -0300

    Added dotenv.

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'f9046a1827799b',
-    pass: '594988fe32df10',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GymPoint <noreply@gympoint.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://c2730970ebd84529929cea8c6f17df21@sentry.io/1844017',
+  dsn: process.env.SENTRY_DSN,
 };
